0e5d004c7e528199e5c6c1bf0164e4138805606b
Eugene <sickbear@mail.ru>
2022-10-12 21:05:39 +0300

    Deploy project on gh pages

--- src/components/UI/UiLoading/UiLoading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import iconLoader from './img/loader.svg'
-import styles from './UiLoading.module.css'
-
-const UiLoading = () => {
-  return <img className={styles.loader} src={iconLoader} alt="Loading..." />
-}
-
-export default UiLoading
--- src/services/changeCssVariables.js
+++
@@ -1,9 +0,0 @@
-export const changeCssVariables = theme => {
-  const root = document.querySelector(':root')
-
-  const cssVariables = ['title', 'bg']
-
-  cssVariables.forEach(item => {
-    root.style.setProperty(`--theme-default-${item}`, `var(--theme-${theme}-${item})`)
-  })
-}
